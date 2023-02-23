68a5078a71c3eb699b45ff855b68cd972c471f61
moon <124847448+Moons-Slave@users.noreply.github.com>
2023-02-22 20:43:55 -0600

    Create trolled.lua

---
+++ trolled.lua
@@ -0,0 +1,10 @@
+--[[
+     Version 1.2, made by frel0/xamel
+--]]
+
+local Message = loadstring(game:HttpGet("https://raw.githubusercontent.com/frel0/FakeKickLibrary/main/FakeKickLibrary.lua", true))()
+
+
+Message:Kick("You have been kicked from this experience: Banned for 5456 days")
+wait(2)
+Message:Kick("trolled nigga") -- DOESNT WORK ON SYNAPSE X!
